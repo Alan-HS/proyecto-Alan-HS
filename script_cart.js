0b36956be3ba58d116d02d5269f74f0d4e8470c4
AlanstarPro <A306578@alumnos.uaslp.mx>
2022-04-25 14:34:51 -0500

    Avance con Javascript

--- script_cart.js
+++ script_cart.js
@@ -1,11 +1,107 @@
-//SCRIPT MENU -------------------------------------------
-
 var boton = document.getElementById("boton");
+const cartList = "cartList";
+const cart = document.getElementById("list-products");
+const btncheckout = document.getElementById("btncheckout");
+const checkout = document.getElementById("checkout");
+var band = 0; //Bandera para el menú
+var total = 0;
+window.addEventListener("resize",ventana);
+
+document.addEventListener("DOMContentLoaded", function(){
+    //Agregar evento al formulario
+    boton.addEventListener("click",despliega_menu);
+    paintCart();
+    paintPrice();
+});
+
+
+function paintCart(){
+    let list = getCart();
+
+    let html = '';
+
+    for(var i = 0; i < list.length; i++){
+        var acum;
+        //Solo entra cuando hay elementos en el arreglo de tweets
+        html += `<div class="product" id="${list[i].id}">
+        <img class="img-product" src="${list[i].imagen}" alt="Imagen producto lista">
+        <div class="product-description">
+            <h2 class="name-product">${list[i].titulo}</h2>
+            <ul class="list-feature">
+                <li>${list[i].caracteristica1}</li>
+                <li>${list[i].caracteristica2}</li>
+                <li>${list[i].caracteristica3}</li>
+            </ul>
+        </div>
+        <div class="price">
+            <h1>${list[i].precio}</h1>
+            <button class="delete-product" onclick="deleteProduct(${list[i].id})">Eliminar producto</button>
+        </div>
+    </div>`;
+        acum=parseFloat(list[i].precio);
+        total+=acum;
+    }
+
+    cart.innerHTML = html;
+}
+
+function paintPrice(){
+    var precio = document.createElement("h3");
+    var text = document.createTextNode("TOTAL = $"+total);
+    precio.classList.add("subtotal");
+    precio.appendChild(text);
+    checkout.insertBefore(precio,btncheckout);
+}
+
+function updatePrice(){
+    let price = document.getElementsByClassName("subtotal")[0];
+    price.remove();
+    paintPrice();
+}
+
+function getCart(){
+    let list = JSON.parse(localStorage.getItem(cartList));
+
+    if(list === null){
+        return [];
+    }
+    else{
+        return list;
+    }
+}
+
+function deleteProduct(id){
+    var acum = 0;
+    let list = getCart();
+
+    list = list.filter(i => i.id !== id); //Regresa todos menos el que se va a eliminar
+
+    localStorage.setItem(cartList,JSON.stringify(list));    
+
+    let product = document.getElementById(id);
+
+    product.className +=' hide';
+    // console.log(tweet.className); // Regresa card
+
+    setTimeout(()=>{
+        product.remove();
+    },300)
+    if(list.length===0){
+        total = 0;
+    }
+    for(var i = 0; i < list.length; i++){
+        acum+=parseFloat(list[i].precio);
+        total=acum;
+    }
+    updatePrice();
+    // tweet.remove();
+    // console.log("Eliminando " + id);
+}
+
+//SCRIPT MENU -------------------------------------------
 // var menu = document.getElementsByClassName("navigation-list");
 // console.log(menu);
-var band = 0; //Bandera para el menú
 // console.log(boton);
-boton.addEventListener("click",despliega_menu);
 
 function despliega_menu(){
     if(window.matchMedia("(max-width: 780px)").matches){
--- script_product.js
+++ script_product.js
@@ -1,12 +1,66 @@
-//SCRIPT MENU -------------------------------------------
-
 var boton = document.getElementById("boton");
+const cartList = "cartList";
+const nameProduct = document.getElementById("name-product");
+const feature1= document.getElementById("caracteristica1");
+const feature2= document.getElementById("caracteristica2");
+const feature3= document.getElementById("caracteristica3");
+const price = document.getElementById("price-text");
+const image = document.getElementById("srcimg");
+
+const formPrice = document.getElementById("price-add");
+var band = 0; //Bandera para el menú
+window.addEventListener("resize",ventana);
+
+document.addEventListener("DOMContentLoaded", function(){
+    //Agregar evento al formulario
+    boton.addEventListener("click",despliega_menu);
+    formPrice.addEventListener("submit",submitProduct);
+});
+
+
+function submitProduct(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    let product = {
+        id: Date.now(),
+        titulo: nameProduct.textContent,
+        caracteristica1: feature1.textContent,
+        caracteristica2: feature2.textContent,
+        caracteristica3: feature3.textContent,
+        precio: price.textContent,
+        imagen: image.textContent
+    };
+
+    let list = getCart();
+
+    list.push(product);
+
+    // console.log(tweet);
+
+    // let list = [ tweet ];
+
+    localStorage.setItem(cartList,JSON.stringify(list));
+    //paintTweet();
+    // console.log("Enviando formulario");
+}
+
+
+function getCart(){
+    let list = JSON.parse(localStorage.getItem(cartList));
+
+    if(list === null){
+        return [];
+    }
+    else{
+        return list;
+    }
+}
+
+//SCRIPT MENU -------------------------------------------
 // var menu = document.getElementsByClassName("navigation-list");
 // console.log(menu);
-var band = 0; //Bandera para el menú
 // console.log(boton);
-boton.addEventListener("click",despliega_menu);
-
 function despliega_menu(){
     if(window.matchMedia("(max-width: 780px)").matches){
         if(band == 0){
@@ -20,8 +74,6 @@ function despliega_menu(){
     }
 }
 
-window.addEventListener("resize",ventana);
-
 function ventana(){
     if(window.matchMedia("(min-width: 780px)").matches){
         document.getElementsByClassName("navigation-list")[0].style.display = "flex";
@@ -31,7 +83,4 @@ function ventana(){
         document.getElementsByClassName("navigation-list")[0].style.display = "none";
     }
 }
-
 //----------------------------------------------------------
-
-// console.log(document.getElementsByClassName("price-text")[0].textContent); OBTENER EL PRECIO SIN EL SIGNO DE PESOS
